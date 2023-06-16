948d075a3fa93592089d0f403e75defc7e53dfa3
overoxidize <overoxidize@protonmail.com>
2023-06-16 09:58:44 -0400

    Currently moving server functionality into the impl block, cleaning up dead struct mocking code.

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,66 @@
-use core::fmt;
-use std::fmt::Error;
 use std::net::{TcpListener, TcpStream, SocketAddr, IpAddr, Ipv4Addr};
 use std::{io, time};
 use std::io::{prelude::*, BufReader};
-use std::rc::Rc;
 use std::thread;
 use uuid::Uuid;
-use anyhow::{anyhow, Result};
 
 
 const MAX_LEN: u32 = 510;
 
 const CR_LF: &str = "\r\n";
 
-const RPL_WELCOME: &str = "001";
-const RPL_YOURHOST: &str = "002";
-const RPL_CREATED: &str = "003";
-const RPL_MYINFO: &str = "004";
-const RPL_LUSERCLIENT: &str = "251";
-const RPL_LUSEROP: &str = "252";
-const RPL_LUSERUNKNOWN: &str = "253";
-const RPL_LUSERCHANNELS: &str = "254";
-const RPL_LUSERME: &str = "255";
-const RPL_AWAY: &str = "301";
-const RPL_UNAWAY: &str = "305";
-const RPL_NOWAWAY: &str = "306";
-const RPL_WHOISUSER: &str = "311";
-const RPL_WHOISSERVER: &str = "312";
-const RPL_WHOISOPERATOR: &str = "313";
-const RPL_WHOISIDLE: &str = "317";
-const RPL_ENDOFWHOIS: &str = "318";
-const RPL_WHOISCHANNELS: &str = "319";
-const RPL_WHOREPLY: &str = "352";
-const RPL_ENDOFWHO: &str = "315";
-const RPL_LIST: &str = "322";
-const RPL_LISTEND: &str = "323";
-const RPL_CHANNELMODEIS: &str = "324";
-const RPL_NOTOPIC: &str = "331";
-const RPL_TOPIC: &str = "332";
-const RPL_NAMREPLY: &str = "353";
-const RPL_ENDOFNAMES: &str = "366";
-const RPL_MOTDSTART: &str = "375";
-const RPL_MOTD: &str = "372";
-const RPL_ENDOFMOTD: &str = "376";
-const RPL_YOUREOPER: &str = "381";
-const ERR_NOSUCHNICK: &str = "401";
-const ERR_NOSUCHSERVER: &str = "402";
-const ERR_NOSUCHCHANNEL: &str = "403";
-const ERR_CANNOTSENDTOCHAN: &str = "404";
-const ERR_NORECIPIENT: &str = "411";
-const ERR_NOTEXTTOSEND: &str = "412";
-const ERR_UNKNOWNCOMMAND: &str = "421";
-const ERR_NOMOTD: &str = "422";
-const ERR_NONICKNAMEGIVEN: &str = "431";
+// const RPL_WELCOME: &str = "001";
+// const RPL_YOURHOST: &str = "002";
+// const RPL_CREATED: &str = "003";
+// const RPL_MYINFO: &str = "004";
+// const RPL_LUSERCLIENT: &str = "251";
+// const RPL_LUSEROP: &str = "252";
+// const RPL_LUSERUNKNOWN: &str = "253";
+// const RPL_LUSERCHANNELS: &str = "254";
+// const RPL_LUSERME: &str = "255";
+// const RPL_AWAY: &str = "301";
+// const RPL_UNAWAY: &str = "305";
+// const RPL_NOWAWAY: &str = "306";
+// const RPL_WHOISUSER: &str = "311";
+// const RPL_WHOISSERVER: &str = "312";
+// const RPL_WHOISOPERATOR: &str = "313";
+// const RPL_WHOISIDLE: &str = "317";
+// const RPL_ENDOFWHOIS: &str = "318";
+// const RPL_WHOISCHANNELS: &str = "319";
+// const RPL_WHOREPLY: &str = "352";
+// const RPL_ENDOFWHO: &str = "315";
+// const RPL_LIST: &str = "322";
+// const RPL_LISTEND: &str = "323";
+// const RPL_CHANNELMODEIS: &str = "324";
+// const RPL_NOTOPIC: &str = "331";
+// const RPL_TOPIC: &str = "332";
+// const RPL_NAMREPLY: &str = "353";
+// const RPL_ENDOFNAMES: &str = "366";
+// const RPL_MOTDSTART: &str = "375";
+// const RPL_MOTD: &str = "372";
+// const RPL_ENDOFMOTD: &str = "376";
+// const RPL_YOUREOPER: &str = "381";
+// const ERR_NOSUCHNICK: &str = "401";
+// const ERR_NOSUCHSERVER: &str = "402";
+// const ERR_NOSUCHCHANNEL: &str = "403";
+// const ERR_CANNOTSENDTOCHAN: &str = "404";
+// const ERR_NORECIPIENT: &str = "411";
+// const ERR_NOTEXTTOSEND: &str = "412";
+// const ERR_UNKNOWNCOMMAND: &str = "421";
+// const ERR_NOMOTD: &str = "422";
+// const ERR_NONICKNAMEGIVEN: &str = "431";
 const ERR_NICKNAMEINUSE: &str = "433";
-const ERR_USERNOTINCHANNEL: &str = "441";
-const ERR_NOTONCHANNEL: &str = "442";
-const ERR_NOTREGISTERED: &str = "451";
-const ERR_NEEDMOREPARAMS: &str = "461";
-const ERR_ALREADYREGISTRED: &str = "462";
-const ERR_PASSWDMISMATCH: &str = "464";
-const ERR_UNKNOWNMODE: &str = "472";
-const ERR_NOPRIVILEGES: &str = "481";
-const ERR_CHANOPRIVSNEEDED: &str = "482";
-const ERR_UMODEUNKNOWNFLAG: &str = "501";
-const ERR_USERSDONTMATCH: &str = "502";
+// const ERR_USERNOTINCHANNEL: &str = "441";
+// const ERR_NOTONCHANNEL: &str = "442";
+// const ERR_NOTREGISTERED: &str = "451";
+// const ERR_NEEDMOREPARAMS: &str = "461";
+// const ERR_ALREADYREGISTRED: &str = "462";
+// const ERR_PASSWDMISMATCH: &str = "464";
+// const ERR_UNKNOWNMODE: &str = "472";
+// const ERR_NOPRIVILEGES: &str = "481";
+// const ERR_CHANOPRIVSNEEDED: &str = "482";
+// const ERR_UMODEUNKNOWNFLAG: &str = "501";
+// const ERR_USERSDONTMATCH: &str = "502";
 
 pub enum Recipient {
     Server,
@@ -72,6 +68,7 @@ pub enum Recipient {
 }
 
 pub enum Commands {
+    Connect,
     Join,
     Part,
     PrivMsg
@@ -96,7 +93,6 @@ struct Server {
 impl Server {
  fn new(channels: Vec<Channel>, 
     connected_users: Vec<User>, 
-    registered_clients: Vec<RegisteredClient>,
     server_name: String,
     socket_addr: SocketAddr,
     nud: Uuid) -> Self {
@@ -114,10 +110,27 @@ impl Server {
 
         }
 
+    fn run(self) {
+        let listener = TcpListener::bind(self.socket_addr).unwrap();
+
+        for stream in listener.incoming() {
+            match stream {
+                Ok(stream) => {
+                    handle_connection(stream);
+                }
+                Err(e) => {
+                    eprintln!("There was a server error {:?}", e);
+                }
+            }
+        }
+    }
+
     fn handle_registration(mut self, client: Client, user: User) -> ServerReply {
+       
         let conn_user_vec: Vec<String> = self.connected_users.iter().map(|user| user.nickname.clone()).collect();
-        
-        let mut srp: ServerReply;
+
+        let srp: ServerReply;
+
         if conn_user_vec.contains(&user.nickname) {
             srp = ServerReply {
                 prefix: ":".to_string(),
@@ -126,8 +139,8 @@ impl Server {
                 param_two: ERR_NICKNAMEINUSE.to_string(),
             };
 
-            
             eprintln!("This username has already been taken");
+
             return srp
         } else {
             srp = ServerReply {
@@ -143,6 +156,26 @@ impl Server {
             srp
         }
     }
+
+    fn handle_sender(mut stream: TcpStream) -> io::Result<()> {
+        let mut buf = [0;512];
+    
+        for _ in 0..1000 {
+            let bytes_read = stream.read(&mut buf)?;
+    
+            if bytes_read == 0 {
+                return Ok(())
+            }
+    
+            stream.write(&buf[..bytes_read])?;
+    
+            println!("from the sender: {}", String::from_utf8_lossy(&buf));
+    
+            thread::sleep(time::Duration::from_secs(1));
+        }
+    
+        Ok(())
+    }
     // let proto_user = User {
     //     nickname: "ProtoUser".to_string(),
     //     client: IrcClient::FooIrc ,
@@ -157,12 +190,9 @@ impl Server {
         
     // };
     
-    // let reg_client = RegisteredClient {
-    //     user_nick: proto_user.nickname,
-    // };
-    // let reg_vec = vec![reg_client];
+
     // let channel_vec = vec![proto_channel.clone()];
-    // let socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),8080);
+    
     // let proto_server =  Server {
     //         channels: channel_vec,
     //         connected_users: user_vec,
@@ -172,7 +202,21 @@ impl Server {
 
     }
 
+fn handle_connection(mut stream: TcpStream) -> io::Result<()>{
+    let buf_reader = BufReader::new(&mut stream);
 
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+        let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+        stream.write_all(response.as_bytes()).unwrap();
+
+        Ok(())
+}
 
 #[derive(Clone, Debug)]
 pub struct User {
@@ -182,7 +226,14 @@ pub struct User {
 }
 
 impl User {
-    pub fn send_message(&self, msg: Message) -> Message {
+    pub fn new(nickname: String, client: IrcClient, full_name: String) -> Self {
+        User {
+            nickname,
+            client: IrcClient::FooIrc,
+            full_name,
+        }
+    }
+    pub fn send_message(&self, msg: UserMessage) -> UserMessage {
         let msg_len = msg.content.len();
         let max_msg_len = MAX_LEN as usize + CR_LF.len();
         
@@ -190,7 +241,7 @@ impl User {
             panic!("Message over character limit")
         }
         
-        let user_msg = Message {
+        let user_msg = UserMessage {
             recipient: "Sender".to_string(),
             sender: "Sender".to_string(),
             command: "".to_string(),
@@ -215,7 +266,7 @@ struct Network {
 
 
 #[derive(Clone)]
-pub struct Message {
+pub struct UserMessage {
     sender: String,
     recipient: String,
     command: String,
@@ -238,12 +289,30 @@ struct Client {
     user: User,
 }
 
+
 impl Client {
+
+    pub fn new(server: Server, user: User) -> Self {
+        Client {
+            server,
+            user
+        }
+    }
+
     pub fn register_client(mut self, user: User, mut server: Server, pass: bool) {
 
-        let connection = TcpStream::connect(server.socket_addr).unwrap();
+        let mut connection = TcpStream::connect(server.socket_addr).unwrap();
 
-        let mut reg_msg = String::new();
+        let nick_msg = String::from("Nick") + &user.nickname.to_string();
+        let user_msg = String::from("Nick") + &user.nickname.to_string();
+        let mut buffer = Vec::new();
+
+        connection.write(nick_msg.as_bytes());
+        connection.write(user_msg.as_bytes());
+
+        connection.read_to_end(&mut buffer);
+
+        let server_resp = String::from_utf8(buffer).unwrap();
 
         // let nick = &user.nickname;
         // let user_info = &user.full_name;
@@ -252,14 +321,8 @@ impl Client {
         //     user_nick: nick.to_string(),
         // };
 
- 
-
-        let srp = ServerReply {
-            prefix: ":".to_string(),
-            num_code: 433,
-            param_one: user.nickname.clone(),
-            param_two: ERR_NICKNAMEINUSE.to_string(),
-        };
+        
+        println!("Server response: {:}", server_resp);
             
         server.connected_users.push(user);
 
@@ -276,28 +339,7 @@ struct ServerReply {
     param_two: String,
 }
 
-impl std::fmt::Display for User {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}, {:?}, {}", self.nickname, self.client, self.full_name)
-    }
-}
 
-
-fn handle_connection(mut stream: TcpStream) -> io::Result<()>{
-    let buf_reader = BufReader::new(&mut stream);
-
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-
-        let response = "HTTP/1.1 200 OK\r\n\r\n";
-
-        stream.write_all(response.as_bytes()).unwrap();
-
-        Ok(())
-}
 
 fn handle_sender(mut stream: TcpStream) -> io::Result<()> {
     let mut buf = [0;512];
@@ -322,6 +364,38 @@ fn handle_sender(mut stream: TcpStream) -> io::Result<()> {
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
+    let proto_user = User {
+        nickname: "ProtoUser".to_string(),
+        client: IrcClient::FooIrc ,
+        full_name: "Proto User".to_string()
+    };
+
+    let user_vec = vec![proto_user.clone()];
+
+    let channel_owner = ChannelCreator(proto_user.nickname.clone());
+
+    let proto_channel = Channel {
+        name: "ProtoChannel".to_string(),
+        users: user_vec.clone(),
+        creator: channel_owner
+        
+    };
+
+    let channel_vec = vec![proto_channel];
+
+    let socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),8080);
+    let server_nud = Uuid::new_v4();
+    let server = Server {
+        channels: channel_vec,
+        connected_users: user_vec,
+        server_name: String::from("ProtoServer"),
+        socket_addr: socket,
+        nud: server_nud
+    };
+    // Either within the function `run` itself, or prior to calling it,
+    // we need to set up writing to stdout.
+    server.run();
+    
     let mut thread_vec: Vec<thread::JoinHandle<()>> = Vec::new();
 
     for stream in listener.incoming() {
